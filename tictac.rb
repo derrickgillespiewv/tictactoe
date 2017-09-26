fbbfced9b1d098d09acc747f24bd21a9e28f3166
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-09-26 14:07:29 -0400

    first test

---
+++ tictac.rb
@@ -0,0 +1,5 @@
+def update_move(board, player_symbol, position)
+
+board = ['x','2','3','4','5','6','7','8','9']
+
+end
--- tictac_test.rb
+++ tictac_test.rb
@@ -6,6 +6,8 @@ class TestTicTac < Minitest::Test
  		assert_equal(1,1)
 	end  
 
-	def test_chomp
-
+	def test_first_spot
+		board = ['1','2','3','4','5','6','7','8','9']
+		assert_equal(['x','2','3','4','5','6','7','8','9'], update_move(board, 'x', '1'))
+	end
 end
