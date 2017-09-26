fb0279abca8441478cd369579054ddc1a239d154
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-09-26 14:48:29 -0400

    legal move

--- tictac.rb
+++ tictac.rb
@@ -1,5 +1,14 @@
-def update_move(board, player_symbol, position)
+def update_move(board, player, position)
+board[position.to_i - 1] = player
+board
+end
 
-board = ['x','2','3','4','5','6','7','8','9']
-
+def spot_open(board, position)
+	if board[position - 1] == 'x' || board[position - 1] == 'o'
+		false
+	elsif position>= 10 || position<= 0
+		false
+	else
+		true
+	end
 end
--- tictac_test.rb
+++ tictac_test.rb
@@ -10,4 +10,36 @@ class TestTicTac < Minitest::Test
 		board = ['1','2','3','4','5','6','7','8','9']
 		assert_equal(['x','2','3','4','5','6','7','8','9'], update_move(board, 'x', '1'))
 	end
+
+	def test_second_spot
+		board = ['x','2','3','4','5','6','7','8','9']
+		assert_equal(['x','2','3','4','5','6','o','8','9'], update_move(board, 'o', '7'))
+	end
+
+	def test_spot_legal
+		board = ['x','2','3','4','5','6','o','8','9']
+		position = 8
+		assert_equal(true, spot_open(board, position)) 
+	end 
+
+
+	def test_spot_ten
+		board = ['x','2','3','4','5','6','o','8','9']
+		position = 10
+		assert_equal(false, spot_open(board, position)) 
+	end 
+
+
+	def test_spot_negative
+		board = ['x','2','3','4','5','6','o','8','9']
+		position = 0
+		assert_equal(false, spot_open(board, position)) 
+	end 
+
+	# def test_plater
+	# 	assert_equal(player1, change_player(player))
+	
 end
+
+
+
