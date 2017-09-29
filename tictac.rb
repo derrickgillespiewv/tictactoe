a9c5722eefb7f53fd88bf5e54f0590c9e0a4a46f
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-09-28 21:35:39 -0400

    Set Character

--- tictac.rb
+++ tictac.rb
@@ -20,21 +20,38 @@ def spot_open(board, position)
 	end
 end
 
-def change_player(current_player)
-	if current_player == 'x'
-		new_player = 'o'
-	elsif current_player == 'o'
-		new_player = 'x'
+def change_player(player)
+	if player == 'x'
+		player = 'o'
+		player 
+	elsif player == 'o'
+		player = 'x'
+		player 
+else
+end
+end
+
+
+def set_character(yes_or_no)
+	until yes_or_no == 'y' or yes_or_no == 'n'
+	p 'Would you like to play Tic-Tac-Toe?(y or n)'
+	yes_or_no = gets.chomp
+	if yes_or_no == 'y'
+	player_choice = ['x','o']
+	player_one = player_choice.delete(player_choice.sample)
+
+	player_two = player_choice.sample
+	p "Player One is #{player_one}"
+	p "Player Two is #{player_two}"
+	player_one
+	elsif yes_or_no == 'n'
+	puts 'See ya next time then'
 	else
-		new_player
-	end
+	print 'Please enter a valid character'
+end
+end
 end
 
-
-def set_game(yes_or_no)
-	board = ['1','2','3','4','5','6','7','8','9']
-	player one 
-end
 
 def test_for_full_board(board)
     if board.all? {|space| space.include?("x") || space.include?("o")}
@@ -43,5 +60,4 @@ def test_for_full_board(board)
         board_full = false
     end
 end
- 
  
